ec85b3cdb6824d059ea2eba1a01d65360fb0dc5e
Yufei-star <52608573+Yufei-star@users.noreply.github.com>
2026-05-28 15:56:14 -0400

    Add files via upload

---
+++ Fig2_growth/all_cell_volume.m
@@ -0,0 +1,8 @@
+function  [V,h] = all_cell_volume(R,Rt,Lseg)
+N=length(R)-1;%# of cells
+il=2:N+1;
+ip=1:N;
+h=(abs(R)-sqrt(R.^2-Rt^2)).*sign(R);
+V_SphCap=1/6*pi*h.*(3*Rt^2+h.^2);
+V=pi*Rt^2.*Lseg+V_SphCap(il)-V_SphCap(ip);
+end
